1329f06bb609bac15eb1069568f7aa772578bb19
Salman Parkar <39862393+iamsalman1403@users.noreply.github.com>
2019-08-20 15:09:04 +0530

    Add files via upload

---
+++ templete.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("#scroll").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 2000, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
